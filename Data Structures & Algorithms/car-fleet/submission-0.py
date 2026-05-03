d88f46d2f3aa39b735ffe66a7bb9eefd94671fd8
Oskar Nowak <ozzy.nowak@gmail.com>
2026-05-03 23:19:51 +0200

    Add: car-fleet - submission-0

---
+++ Data Structures & Algorithms/car-fleet/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        cars = sorted(zip(position, speed), reverse=True)
+
+        stack = []
+        print(cars)
+
+        for i in range(len(cars)):
+            if stack and(target - stack[-1][0]) / stack[-1][1] >= (target - cars[i][0]) / cars[i][1]:
+                continue
+            else:
+                stack.append(cars[i])
+
+        return len(stack)
